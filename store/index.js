91d1e2704b29afa912982c17192a5a5866e9615d
wqy <1950016253@qq.com>
2020-10-25 22:14:58 +0800

    引入封装的请求库和Vuex

---
+++ store/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+import $H from '../common/request.js'
+
+export default new Vuex.Store({
+	state: {
+		user: null,
+		token: null
+	},
+	actions: {
+		logout({
+			state
+		}) {
+			$H.post('/logout', {}, {
+				token: true
+			})
+			state.user = null
+			state.token = null
+			uni.removeStorageSync('user')
+			uni.removeStorageSync('token')
+
+			uni.reLaunch({
+				url: '/pages/login/login'
+			})
+		},
+		login({
+			state
+		}, user) {
+			state.user = user
+			state.token = user.token
+
+			uni.setStorageSync('user', JSON.stringify(user))
+			uni.setStorageSync('token', user.token)
+		},
+		intUser({
+			state
+		}) {
+			let user = uni.getStorageSync('user')
+			if (user) {
+				state.user = JSON.parse(user)
+				state.token = state.user.token
+			}
+		},
+		updateSize({
+			state
+		}, e) {
+			state.user.total_size = e.total_size
+			state.user.user_size = e.used_size
+		}
+	}
+})
